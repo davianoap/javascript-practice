6cca778d7449316799db1fed2779363def0b539e
Daviano Anugrah .P <daviano123@gmail.com>
2023-02-01 18:51:09 +0700

    latian sanbercode 1

---
+++ kuisjs.js
@@ -0,0 +1,18 @@
+let namalengkap;
+namalengkap = "Daviano Anugrah Pratama";
+
+console.log(namalengkap)
+
+let word = 'javascript ';
+let second = 'is ';
+let third = 'awesome';
+
+let outputGabunganVariable = word + second + third;
+console.log(outputGabunganVariable)
+
+let hello = 'helllo'
+let world = 'world'
+
+let output = `${hello} ${world}`
+
+console.log(output);
